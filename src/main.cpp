ca6a0a7bea4d32d58349470f39615472749e336a
markiyanvoytiv <Markiyan.Voytiv@zuman.com>
2026-09-04 18:09:08 +0300

    Implement superloop LED blinking

--- src/main.cpp
+++ src/main.cpp
@@ -1,133 +1,49 @@
 #include <Arduino.h>
 
-volatile bool buttonPressed = false;
-
 struct Config
 {
-    static constexpr uint8_t LED_PIN = 4;
-    static constexpr uint8_t BUTTON_PIN = 10;
-    static constexpr unsigned long BLINK_DELAY_MS = 1000;
-    static constexpr unsigned long DEBOUNCE_DELAY_MS = 200;
-};
+    static constexpr uint8_t GREEN_LED_PIN = 12;
+    static constexpr uint8_t BLUE_LED_PIN = 8;
+    static constexpr uint8_t RED_LED_PIN = 6;
 
-enum class LedState
-{
-    Off,
-    On
-};
-
-enum class LedMode
-{
-    Blinking,
-    On,
-    Off
-};
-
-class Led
-{
-public:
-    static void init()
-    {
-        pinMode(Config::LED_PIN, OUTPUT);
-        set(LedState::Off);
-        pinMode(Config::BUTTON_PIN, INPUT_PULLUP);
-        attachInterrupt(
-            digitalPinToInterrupt(Config::BUTTON_PIN),
-            buttonIsr,
-            FALLING);
-    }
-
-    static void buttonIsr()
-    {
-        buttonPressed = true;
-    }
-
-    static void set(LedState state)
-    {
-        if (state == LedState::On)
-        {
-            digitalWrite(Config::LED_PIN, HIGH);
-        }
-        else
-        {
-            digitalWrite(Config::LED_PIN, LOW);
-        }
-    }
+    static constexpr unsigned long GREEN_BLINK_DELAY_MS = 200;
+    static constexpr unsigned long BLUE_BLINK_DELAY_MS = 500;
+    static constexpr unsigned long RED_BLINK_DELAY_MS = 1000;
 };
 
 void setup()
 {
-    Serial.begin(115200);
-    Led::init();
+    pinMode(Config::GREEN_LED_PIN, OUTPUT);
+    pinMode(Config::BLUE_LED_PIN, OUTPUT);
+    pinMode(Config::RED_LED_PIN, OUTPUT);
+
+    digitalWrite(Config::GREEN_LED_PIN, LOW);
+    digitalWrite(Config::BLUE_LED_PIN, LOW);
+    digitalWrite(Config::RED_LED_PIN, LOW);
 }
 
 void loop()
 {
-    unsigned long startTime = micros();
-    static uint32_t totalLoopTime = 0;
-    static unsigned long previousTime = 0;
-    static LedState currentState = LedState::Off;
-    static LedMode currentMode = LedMode::Blinking;
-    static uint16_t loopCounter = 0;
-    static unsigned long lastButtonPressTime = 0;
+    unsigned long currentTime = millis();
+    static unsigned long greenBlinkStartTime = currentTime;
+    static unsigned long blueBlinkStartTime = currentTime;
+    static unsigned long redBlinkStartTime = currentTime;
 
-    if (buttonPressed)
+    if (currentTime - greenBlinkStartTime >= Config::GREEN_BLINK_DELAY_MS)
     {
-        buttonPressed = false;
-        if (millis() - lastButtonPressTime > Config::DEBOUNCE_DELAY_MS)
-        { 
-            switch (currentMode)
-            {
-            case LedMode::Blinking:
-                currentMode = LedMode::On;
-                break;
-            case LedMode::On:
-                currentMode = LedMode::Off;
-                break;
-            case LedMode::Off:
-                currentMode = LedMode::Blinking;
-                break;
-            }
-            lastButtonPressTime = millis();
-        }
+        digitalWrite(Config::GREEN_LED_PIN, !digitalRead(Config::GREEN_LED_PIN));
+        greenBlinkStartTime = currentTime;
     }
 
-    if (currentMode == LedMode::On)
+    if (currentTime - blueBlinkStartTime >= Config::BLUE_BLINK_DELAY_MS)
     {
-        currentState = LedState::On;
-        Led::set(currentState);
-    }
-    else if (currentMode == LedMode::Blinking)
-    {
-        if (millis() - previousTime >= Config::BLINK_DELAY_MS)
-        {
-            previousTime = millis();
-
-            currentState =
-                (currentState == LedState::Off)
-                    ? LedState::On
-                    : LedState::Off;
-
-            Led::set(currentState);
-        }
-    }
-    else if (currentMode == LedMode::Off)
-    {
-        currentState = LedState::Off;
-        Led::set(currentState);
+        digitalWrite(Config::BLUE_LED_PIN, !digitalRead(Config::BLUE_LED_PIN));
+        blueBlinkStartTime = currentTime;
     }
 
-    unsigned long endTime = micros();
-    unsigned long loopDuration = endTime - startTime;
-
-    totalLoopTime += loopDuration;
-    loopCounter++;
-
-    if (loopCounter == 1000)
+    if (currentTime - redBlinkStartTime >= Config::RED_BLINK_DELAY_MS)
     {
-        Serial.println("Average loop time (microseconds):");
-        Serial.println(totalLoopTime / loopCounter);
-        loopCounter = 0;
-        totalLoopTime = 0;
+        digitalWrite(Config::RED_LED_PIN, !digitalRead(Config::RED_LED_PIN));
+        redBlinkStartTime = currentTime;
     }
 }
