924270e6db62999b2373dfa74e630ea1477dcdc7
markiyanvoytiv <Markiyan.Voytiv@zuman.com>
2026-09-11 11:32:01 +0300

    add timer basic logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,36 @@
 #include <Arduino.h>
+#define RELAY_CONTROL_OUT 8
+#define ON_TIME_US 2000000
+#define OFF_TIME_US 6000000
 
-struct Config
-{
-    static constexpr uint8_t BUTTON_PIN = 16;
-    static constexpr uint32_t POLL_INTERVAL_MS = 10;
-    static constexpr uint8_t STABLE_READS_REQUIRED = 3;
-};
+hw_timer_t * timer = NULL;
 
-enum class ButtonState
+void IRAM_ATTR onTimer()
 {
-    Released,
-    DebouncingPress,
-    Pressed,
-    DebouncingRelease
-};
+    bool relayState = digitalRead(RELAY_CONTROL_OUT);
+    digitalWrite(RELAY_CONTROL_OUT, !relayState);
+    timerAlarmWrite(timer, relayState ? OFF_TIME_US : ON_TIME_US, true);
+}
 
 void setup()
 {
-    Serial.begin(115200);
+    pinMode(RELAY_CONTROL_OUT, OUTPUT);
+    digitalWrite(RELAY_CONTROL_OUT, LOW);
 
-    pinMode(Config::BUTTON_PIN, INPUT_PULLUP);
+    // Initialize timer 0 with prescaler 80, counting up
+    timer = timerBegin(0, 80, true);
+
+    // Attach interrupt handler
+    timerAttachInterrupt(timer, &onTimer, true);
+
+    // Set initial alarm interval
+    timerAlarmWrite(timer, OFF_TIME_US, true);
+
+    // Enable timer alarm
+    timerAlarmEnable(timer);
 }
 
 void loop()
 {
-    static ButtonState buttonState = ButtonState::Released;
-    static uint32_t buttonPressCount = 0;
-    static uint32_t lastPollTime = 0;
-    static uint8_t stableReadCount = 0;
 
-    uint32_t currentTime = millis();
-
-    if (currentTime - lastPollTime < Config::POLL_INTERVAL_MS)
-    {
-        return;
-    }
-
-    lastPollTime = currentTime;
-
-    bool buttonPressed = digitalRead(Config::BUTTON_PIN) == LOW;
-
-    switch (buttonState)
-    {
-        case ButtonState::Released:
-            if (buttonPressed)
-            {
-                stableReadCount = 1;
-                buttonState = ButtonState::DebouncingPress;
-            }
-            break;
-
-        case ButtonState::DebouncingPress:
-            if (!buttonPressed)
-            {
-                stableReadCount = 0;
-                buttonState = ButtonState::Released;
-            }
-            else if (++stableReadCount >= Config::STABLE_READS_REQUIRED)
-            {
-                stableReadCount = 0;
-                buttonState = ButtonState::Pressed;
-
-                buttonPressCount++;
-                Serial.println(buttonPressCount);
-            }
-            break;
-
-        case ButtonState::Pressed:
-            if (!buttonPressed)
-            {
-                stableReadCount = 1;
-                buttonState = ButtonState::DebouncingRelease;
-            }
-            break;
-
-        case ButtonState::DebouncingRelease:
-            if (buttonPressed)
-            {
-                stableReadCount = 0;
-                buttonState = ButtonState::Pressed;
-            }
-            else if (++stableReadCount >= Config::STABLE_READS_REQUIRED)
-            {
-                stableReadCount = 0;
-                buttonState = ButtonState::Released;
-            }
-            break;
-    }
 }
